af0c99f2522103643a723293c15327d5000174eb
Rasif Rustamov <19rustamov1996@gmail.com>
2022-03-28 18:27:30 +0300

    Add combineReducers and unsubscribe functions

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 import store from './redux.js';
 
 const counter = document.querySelector('.count');
+const age = document.querySelector('.age');
 const paragraph = document.querySelector('.paragraph');
+const subscribeButton = document.querySelector('.subscribeButton');
+const unsubscribeButton = document.querySelector('.unsubscribeButton');
 
 const handleIncrease = () => {
 	store.dispatch({type: 'increase'});
 };
 
 const listener = state => {
-	counter.innerHTML = state;
+	counter.innerHTML = state.count;
+	age.innerHTML = state.age;
 };
 
-store.subscribe(listener);
+const handleSubscribe = () => {
+	store.subscribe(listener);
+};
+
+const handleUnsubscribe = () => {
+	store.unsubscribe(listener);
+};
 
 paragraph.addEventListener('click', handleIncrease);
+subscribeButton.addEventListener('click', handleSubscribe);
+unsubscribeButton.addEventListener('click', handleUnsubscribe);
